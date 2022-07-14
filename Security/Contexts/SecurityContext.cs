ebaa46ee1d4dce294a31072b6dc626f321ee0338
ahmadkandil <A77mad.22andil@gmail.com>
2022-07-14 10:37:52 +0200

    Security

---
+++ Security/Contexts/SecurityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Security.Contexts
+{
+    public class SecurityContext : IdentityDbContext<IdentityUser>
+    {
+        public SecurityContext(DbContextOptions<SecurityContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
